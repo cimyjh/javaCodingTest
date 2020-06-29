645bbd7273b7bf017f9a8cfac3471cecaadcb72b
cimyjh <cimyjh@gmail.com>
2020-06-29 15:32:58 +0900

    200629

---
+++ TrappingRainWater.java
@@ -0,0 +1,9 @@
+
+public class TrappingRainWater{
+
+    public static void main(String[] args){
+        int[] nums = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(trap(nums));
+    }
+    public
+}
